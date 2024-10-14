f261e6ca5cef832a65eaf58d2edadeea2cd1d36b
Andrei Vilkevich <andreivilkevich@gmail.com>
2024-10-14 21:51:44 +0300

    Make `book-service` and `library-service` use the service registry

--- book-service/build.gradle.kts
+++ book-service/build.gradle.kts
@@ -1,15 +1,16 @@
 group = "org.example"
 version = "0.1.0"
 
-val lombokVersion by extra("1.18.34")
-val mapstructVersion by extra("1.6.2")
-val postgresqlVersion by extra("42.7.4")
-val springBootVersion by extra("3.3.4")
-val springCloudVersion by extra("4.1.3")
-val springDocVersion by extra("2.6.0")
+extra["lombokVersion"] = "1.18.34"
+extra["mapstructVersion"] = "1.6.2"
+extra["postgresqlVersion"] = "42.7.4"
+extra["springBootVersion"] = "3.3.4"
+extra["springCloudVersion"] = "2023.0.3"
+extra["springDocVersion"] = "2.6.0"
 
 plugins {
     id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
     id("java")
 }
 
@@ -18,23 +19,30 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter:$springBootVersion")
-    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
-    implementation("org.springframework.boot:spring-boot-starter-validation:$springBootVersion")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter:${property("springBootVersion")}")
+    implementation("org.springframework.boot:spring-boot-starter-web:${property("springBootVersion")}")
+    implementation("org.springframework.boot:spring-boot-starter-validation:${property("springBootVersion")}")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:${property("springBootVersion")}")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:$springCloudVersion")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocVersion")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springDocVersion")}")
 
-    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+    implementation("org.mapstruct:mapstruct:${property("mapstructVersion")}")
 
-    runtimeOnly("org.postgresql:postgresql:$postgresqlVersion")
+    runtimeOnly("org.postgresql:postgresql:${property("postgresqlVersion")}")
 
-    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    compileOnly("org.projectlombok:lombok:${property("lombokVersion")}")
 
-    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
-    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+    annotationProcessor("org.projectlombok:lombok:${property("lombokVersion")}")
+    annotationProcessor("org.mapstruct:mapstruct-processor:${property("mapstructVersion")}")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 java {
