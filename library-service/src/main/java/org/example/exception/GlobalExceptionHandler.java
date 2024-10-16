ada1962f26e8f5141b0eccf9d6b7f6ff25ce6c21
Andrei Vilkevich <andreivilkevich@gmail.com>
2024-10-16 17:07:10 +0300

    library-service: add `GlobalExceptionHandler`
    The `GlobalExceptionHandler` class is responsible for handling
    exceptions thrown by the controller.

---
+++ library-service/src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package org.example.exception;
+
+import org.example.dto.ErrorResponse;
+import org.example.mapper.ErrorMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleBookNotFound(BookNotFoundException e) {
+        ErrorResponse resp = ErrorMapper.INSTANCE.toErrorResponse(e);
+        return ResponseEntity.status(resp.getStatus()).body(resp);
+    }
+
+}
