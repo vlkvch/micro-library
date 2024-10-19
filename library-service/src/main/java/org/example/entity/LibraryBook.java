f6e413b5c41a4006ecf54c87ef85680b63d5115b
Andrei Vilkevich <andreivilkevich@gmail.com>
2024-10-19 12:41:45 +0300

    library-service: add Lombok's `@Builder` annotation to the `LibraryBook` entity
    The `@Builder` annotation requires having `@NoArgsConstructor` and
    `@AllArgsConstructor` as well.

--- library-service/src/main/java/org/example/entity/LibraryBook.java
+++ library-service/src/main/java/org/example/entity/LibraryBook.java
@@ -6,10 +6,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "library_books")
 public class LibraryBook {
 
